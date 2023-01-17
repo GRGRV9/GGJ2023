8c96a970e538133f18bc5294fc68f57a25dd9cfc
GRGRV9 <niamod@mail.ru>
2023-01-17 15:45:25 +0600

    fix animation

--- Assets/Scripts/Character/MovementController.cs
+++ Assets/Scripts/Character/MovementController.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public float moveSpeed = 2f;
+    public float startSpeed;
+    private float moveSpeed;
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        moveSpeed = startSpeed;
     }
 
     void FixedUpdate()
@@ -17,5 +21,23 @@ public class MovementController : MonoBehaviour
         float moveY = 0;
 
         rb.velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+
+        animator.SetFloat("speed", moveSpeed);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown("s"))
+        {
+            moveSpeed = 0f;
+            print(moveSpeed);
+        }
+
+        if (Input.GetKeyUp("s"))
+        {
+            moveSpeed = startSpeed;
+            print(moveSpeed);
+        }
     }
 }
