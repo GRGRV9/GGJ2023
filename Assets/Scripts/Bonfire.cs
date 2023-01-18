6ad9bcfd6dd8a4c35555a3a80dea8c48a9bc465c
GRGRV9 <niamod@mail.ru>
2023-01-18 22:42:39 +0600

    add fire and ice mechanic

---
+++ Assets/Scripts/Bonfire.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    bool isActive = true;
+    public GameObject bon1;
+    public GameObject bon2;
+    public GameObject bon3;
+    public GameObject bon4;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isActive==true)
+        {
+            collision.gameObject.GetComponent<MovementController>().Dying();
+        }
+
+        if (collision.gameObject.tag == "ice")
+        {
+            bon1.SetActive(false);
+            bon2.SetActive(false);
+            bon3.SetActive(false);
+            bon4.SetActive(false);
+            isActive = false;
+            Debug.Log("bonfire deactivated");
+        }
+        
+    }
+}
